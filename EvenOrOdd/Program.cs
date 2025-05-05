ae71c84163f35336a9ca3094fa8debedfcf9f515
Zalewa <zalewski.ba@gmail.com>
2025-05-05 23:45:18 +0200

    first commit

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            var number = GetInput();
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Liczba parzysta");
+            }
+            else
+            {
+                Console.WriteLine("Liczba nieparzysta");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int GetInput() 
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out var number))
+                {
+                    Console.WriteLine("\nSpróbuj ponownie");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
